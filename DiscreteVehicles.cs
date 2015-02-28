0f5a13e6fb754ec08428f28bbab994c85997003f
dario-wat <dario.vidas92@gmail.com>
2015-02-28 15:44:50 +0100

    First add

---
+++ DiscreteVehicles.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DiscreteVehicles : MonoBehaviour {
+
+	// Name of the file which contains a map
+	public string filename;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private static void readFile(string filename) {
+
+	}
+}
