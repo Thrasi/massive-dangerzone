faae24ca7deeec0a5ff8a34eaf91ea470f28bea4
dario-wat <dario.vidas92@gmail.com>
2015-03-01 00:06:41 +0100

    Test generating vehicles

--- DiscreteVehicles.cs
+++ DiscreteVehicles.cs
@@ -3,13 +3,25 @@ using System.Collections;
 
 public class DiscreteVehicles : MonoBehaviour {
 
+	// Object to use for vehicles
+	public GameObject vehicle;
+
 	// Name of the file which contains a map
 	public string filename;
 
+
 	// Use this for initialization
 	void Start () {
-		DiscreteMap map = new DiscreteMap(filename);
-		
+		//DiscreteMap map = new DiscreteMap(filename);
+		GameObject v = Instantiate(vehicle) as GameObject;
+		v.SetActive(true);
+		print(v.renderer.material.color);
+		v.renderer.material.color = Color.green;
+		print(v.renderer.material.color);
+		v = Instantiate(vehicle) as GameObject;
+		v.transform.position = new Vector3(2, 1, 2);
+		print(v.renderer.material.color);
+		v.SetActive(true);
 	}
 	
 	// Update is called once per frame
