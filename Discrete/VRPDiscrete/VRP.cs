2075f7bffa3e9a388a81f5f259df030e9d6800ad
Thrasi <thrasi@server.fake>
2015-03-23 15:29:42 +0100

    vrp

---
+++ Discrete/VRPDiscrete/VRP.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class VRP  {
+	public float[,] distances;
+	public int N, V;
+
+	public VRP(int N, int V, float[,] dists) {
+		this.N = N;
+		this.V = V;
+		distances = dists;
+	}
+}
